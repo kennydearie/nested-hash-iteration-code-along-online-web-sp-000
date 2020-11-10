8cb94acb26bc2ad8cc8aeb1558e12c8741a78149
Nkenge Simms <simmsn@yahoo.com>
2020-11-10 16:06:48 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,26 +15,3 @@ require 'pry'
    }
   
 def remove_strawberry(contacts)
-contacts.each do |person, contact_details_hash|
-  
-  
-end
-end
-
-contacts.each do |person, contact_details_hash|
-  if person == "Freddy Mercury"
-    contact_details_hash.each do |attribute, data|
-      binding.pry
-    end
-  end
-end
-
-contacts.each do |person, contact_details_hash|
-  if person == "Freddy Mercury"
-    contact_details_hash.each do |attribute, data|
-      if attribute == :favorite_ice_cream_flavors
-        binding.pry
-      end
-    end
-  end
-end
